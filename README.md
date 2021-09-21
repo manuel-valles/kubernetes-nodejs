9d2d7dc92a33a2cc8955680d888b36e1eb20c72e
Manuel Valles <manukempo@gmail.com>
2021-09-21 09:04:26 +0100

    First approach MongoDB

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # Kuberenetes & NodeJS
+
 An Express app deployed as a container in Kubernetes.
 
-
 ## Basic setup
+
 1. ESLint, Prettier & Airbnb Setup
-    - `$ npm i -D eslint prettier eslint-plugin-prettier eslint-config-prettier eslint-plugin-node eslint-config-node`
-    - `$ npx install-peerdeps --dev eslint-config-airbnb`
-    - Reference:
-        - [ESLint Rules](https://eslint.org/docs/rules/)
-        - [Prettier Options](https://prettier.io/docs/en/options.html)
-        - [Airbnb Style Guide](https://github.com/airbnb/javascript)
+   - `$ npm i -D eslint prettier eslint-plugin-prettier eslint-config-prettier eslint-plugin-node eslint-config-node`
+   - `$ npx install-peerdeps --dev eslint-config-airbnb`
+   - Reference:
+     - [ESLint Rules](https://eslint.org/docs/rules/)
+     - [Prettier Options](https://prettier.io/docs/en/options.html)
+     - [Airbnb Style Guide](https://github.com/airbnb/javascript)
 2. Express, Pug and Tachyons
-    - `$ npm i express pug`
-    - [Tachyons](https://tachyons.io/)
+   - `$ npm i express pug`
+   - [Tachyons](https://tachyons.io/)
+3. MongoDB
+   - [Server](https://docs.mongodb.com/guides/server/install/)
--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const path = require('path');
+const { MongoClient } = require('mongodb');
 const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/dev';
+
+const initMongo = async () => {
+  console.log('Initialising MongoDB...');
+  return MongoClient.connect(
+    mongoURL,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    (err, client) => {
+      if (err) {
+        console.log('Error connecting to MongoDB, retrying in 1 second');
+        return setTimeout(() => initMongo(), 1000);
+      }
+      console.log('MongoDB initialised');
+      return client.db(client.s.options.dbName).collection('notes');
+    }
+  );
+};
 
 const start = async () => {
+  const db = await initMongo();
+
   app.set('view engine', 'pug');
   app.set('views', path.join(__dirname, 'views'));
   app.use(express.static(path.join(__dirname, 'public')));
@@ -13,7 +36,7 @@ const start = async () => {
     console.log(`App listening on http://localhost:${port}`);
   });
 
-  app.get('/', (req, res) => res.render('index', { notes: [] }));
+  app.get('/', async (req, res) => res.render('index', { notes: [] }));
 };
 
 start();
